d0edc72dad80121c27e69a5d96e632a88c1697b5
LarsN280 <larsn280@hotmail.com>
2024-07-08 19:11:24 +0200

    Removed unussed code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'logger.dart';
 import 'package:nes_for_gains/database/database.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
@@ -6,19 +5,7 @@ import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Isar
-  // ignore: unused_local_variable
   final isar = await setupIsar();
-
-  // Check if Isar initialization was successful
-  // ignore: unnecessary_null_comparison
-  if (isar != null) {
-    // ignore: avoid_print
-    logger.i('Isar initialization successful!');
-  } else {
-    // ignore: avoid_print
-    logger.w('Failed to initialize Isar.');
-  }
 
   runApp(App(isar: isar));
 }
