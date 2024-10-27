637c03d76cc9488dc92dd917356ec39c19dd0984
Larsn280 <larsn280@hotmail.com>
2024-10-27 16:21:52 +0100

    OrganisedTest

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,11 +51,12 @@ class _HomeScreenState extends State<HomeScreen> {
                           style: TextStyle(fontWeight: FontWeight.bold)),
                       SizedBox(height: 8.0),
                       Text(
-                        '''NESForGains is your personal companion on the journey to better health and fitness. Track your workouts, monitor your nutrition, and discover new recipes—all in one place. With NESForGains, you can effortlessly log your progress, helping you stay focused and motivated every step of the way.
-
-Add your latest workout to build strength over time, keep tabs on your meals to balance your nutrition, or explore new recipes to fuel your goals. NESForGains makes it easy to see your achievements and plan for tomorrow’s gains.
-
-Let’s keep leveling up—one rep, one meal, and one day at a time!''',
+                        'NESForGains is your personal companion on the journey to better health and fitness. '
+                        'Track your workouts, monitor your nutrition, and discover new recipes—all in one place. '
+                        'With NESForGains, you can effortlessly log your progress, helping you stay focused and motivated every step of the way.\n\n'
+                        'Add your latest workout to build strength over time, keep tabs on your meals to balance your nutrition, '
+                        'or explore new recipes to fuel your goals. NESForGains makes it easy to see your achievements and plan for tomorrow’s gains.\n\n'
+                        'Let’s keep leveling up—one rep, one meal, and one day at a time!',
                         textAlign: TextAlign.center,
                       ),
                     ],
