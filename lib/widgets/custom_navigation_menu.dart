a1889efe3a11c86ed2fb90d4388a61f7f61f0949
Larsn280 <larsn280@hotmail.com>
2024-11-10 17:33:54 +0100

    GestureDetectorAddedToNavMenu

--- lib/widgets/custom_navigation_menu.dart
+++ lib/widgets/custom_navigation_menu.dart
@@ -43,168 +43,179 @@ class CustomNavigationMenuState extends State<CustomNavigationMenu> {
     var offset = renderBox.localToGlobal(Offset.zero);
 
     return OverlayEntry(
-      builder: (context) => Positioned(
-        left: offset.dx,
-        top: offset.dy + size.height,
-        width: 200, // Set width of the overlay menu
-        child: Material(
-          color: Colors.transparent,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              buildMenuOption(
-                icon: Icons.home,
-                label: 'Home',
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/');
-                  _removeOverlay();
-                },
-              ),
-              buildMenuOption(
-                icon: Icons.dining,
-                label: 'Nutrition',
-                onPressed: () {
-                  setState(
-                    () {
-                      if (_isNutritionOpen == false) {
-                        _isNutritionOpen = true;
-                      } else {
-                        _isNutritionOpen = false;
-                      }
-                      _removeOverlay();
-                      _showOverlay();
-                    },
-                  );
-                },
-                hasSubcategories: true,
-                isOpen: _isNutritionOpen,
-              ),
-              _isNutritionOpen == true
-                  ? Column(
+        builder: (context) => Stack(
+              children: [
+                GestureDetector(
+                  onTap: _removeOverlay,
+                  child: Container(
+                    color: Colors.transparent,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                  ),
+                ),
+                Positioned(
+                  left: offset.dx,
+                  top: offset.dy + size.height,
+                  width: 200, // Set width of the overlay menu
+                  child: Material(
+                    color: Colors.transparent,
+                    child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         buildMenuOption(
-                          icon: Icons.calculate,
-                          label: 'Nutrition Calculator',
+                          icon: Icons.home,
+                          label: 'Home',
                           onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, '/nutritionScreen');
+                            Navigator.pushReplacementNamed(context, '/');
                             _removeOverlay();
                           },
-                          color: subColor,
                         ),
                         buildMenuOption(
-                            icon: Icons.list,
-                            label: 'Display Nutrition',
-                            onPressed: () {
-                              Navigator.pushReplacementNamed(
-                                  context, '/displaynutritionScreen');
-                              _removeOverlay();
-                            },
-                            color: subColor),
-                      ],
-                    )
-                  : const Column(),
-              buildMenuOption(
-                icon: Icons.bar_chart,
-                label: 'Workouts',
-                onPressed: () {
-                  setState(() {
-                    if (_isWorkoutOpen == false) {
-                      _isWorkoutOpen = true;
-                    } else {
-                      _isWorkoutOpen = false;
-                    }
-                    _removeOverlay();
-                    _showOverlay();
-                  });
-                },
-                hasSubcategories: true,
-                isOpen: _isWorkoutOpen,
-              ),
-              _isWorkoutOpen == true
-                  ? Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
+                          icon: Icons.dining,
+                          label: 'Nutrition',
+                          onPressed: () {
+                            setState(
+                              () {
+                                if (_isNutritionOpen == false) {
+                                  _isNutritionOpen = true;
+                                } else {
+                                  _isNutritionOpen = false;
+                                }
+                                _removeOverlay();
+                                _showOverlay();
+                              },
+                            );
+                          },
+                          hasSubcategories: true,
+                          isOpen: _isNutritionOpen,
+                        ),
+                        _isNutritionOpen == true
+                            ? Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  buildMenuOption(
+                                    icon: Icons.calculate,
+                                    label: 'Nutrition Calculator',
+                                    onPressed: () {
+                                      Navigator.pushReplacementNamed(
+                                          context, '/nutritionScreen');
+                                      _removeOverlay();
+                                    },
+                                    color: subColor,
+                                  ),
+                                  buildMenuOption(
+                                      icon: Icons.list,
+                                      label: 'Display Nutrition',
+                                      onPressed: () {
+                                        Navigator.pushReplacementNamed(
+                                            context, '/displaynutritionScreen');
+                                        _removeOverlay();
+                                      },
+                                      color: subColor),
+                                ],
+                              )
+                            : const Column(),
                         buildMenuOption(
-                          icon: Icons.fitness_center,
-                          label: 'Add Workout',
+                          icon: Icons.bar_chart,
+                          label: 'Workouts',
+                          onPressed: () {
+                            setState(() {
+                              if (_isWorkoutOpen == false) {
+                                _isWorkoutOpen = true;
+                              } else {
+                                _isWorkoutOpen = false;
+                              }
+                              _removeOverlay();
+                              _showOverlay();
+                            });
+                          },
+                          hasSubcategories: true,
+                          isOpen: _isWorkoutOpen,
+                        ),
+                        _isWorkoutOpen == true
+                            ? Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  buildMenuOption(
+                                    icon: Icons.fitness_center,
+                                    label: 'Add Workout',
+                                    onPressed: () {
+                                      Navigator.pushReplacementNamed(
+                                          context, '/addworkoutScreen');
+                                      _removeOverlay();
+                                    },
+                                    color: subColor,
+                                  ),
+                                  buildMenuOption(
+                                    icon: Icons.list,
+                                    label: 'Display Workouts',
+                                    onPressed: () {
+                                      Navigator.pushReplacementNamed(
+                                          context, '/displayworkoutScreen');
+                                      _removeOverlay();
+                                    },
+                                    color: subColor,
+                                  ),
+                                ],
+                              )
+                            : const Column(),
+                        buildMenuOption(
+                          icon: Icons.receipt,
+                          label: 'Recipes',
+                          onPressed: () {
+                            setState(() {
+                              if (_isRecipeOpen == false) {
+                                _isRecipeOpen = true;
+                              } else {
+                                _isRecipeOpen = false;
+                              }
+                              _removeOverlay();
+                              _showOverlay();
+                            });
+                          },
+                          hasSubcategories: true,
+                          isOpen: _isRecipeOpen,
+                        ),
+                        _isRecipeOpen == true
+                            ? Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  buildMenuOption(
+                                      icon: Icons.food_bank,
+                                      label: 'Add Recipe',
+                                      onPressed: () {
+                                        Navigator.pushReplacementNamed(
+                                            context, '/addrecipeScreen');
+                                        _removeOverlay();
+                                      },
+                                      color: subColor),
+                                  buildMenuOption(
+                                      icon: Icons.list,
+                                      label: 'Recipelist',
+                                      onPressed: () {
+                                        Navigator.pushReplacementNamed(
+                                            context, '/displayrecipeScreen');
+                                        _removeOverlay();
+                                      },
+                                      color: subColor),
+                                ],
+                              )
+                            : const Column(),
+                        buildMenuOption(
+                          icon: Icons.book_sharp,
+                          label: 'Book of Exuses',
                           onPressed: () {
                             Navigator.pushReplacementNamed(
-                                context, '/addworkoutScreen');
+                                context, '/bookofexusesScreen');
                             _removeOverlay();
                           },
-                          color: subColor,
-                        ),
-                        buildMenuOption(
-                          icon: Icons.list,
-                          label: 'Display Workouts',
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, '/displayworkoutScreen');
-                            _removeOverlay();
-                          },
-                          color: subColor,
                         ),
                       ],
-                    )
-                  : const Column(),
-              buildMenuOption(
-                icon: Icons.receipt,
-                label: 'Recipes',
-                onPressed: () {
-                  setState(() {
-                    if (_isRecipeOpen == false) {
-                      _isRecipeOpen = true;
-                    } else {
-                      _isRecipeOpen = false;
-                    }
-                    _removeOverlay();
-                    _showOverlay();
-                  });
-                },
-                hasSubcategories: true,
-                isOpen: _isRecipeOpen,
-              ),
-              _isRecipeOpen == true
-                  ? Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        buildMenuOption(
-                            icon: Icons.food_bank,
-                            label: 'Add Recipe',
-                            onPressed: () {
-                              Navigator.pushReplacementNamed(
-                                  context, '/addrecipeScreen');
-                              _removeOverlay();
-                            },
-                            color: subColor),
-                        buildMenuOption(
-                            icon: Icons.list,
-                            label: 'Recipelist',
-                            onPressed: () {
-                              Navigator.pushReplacementNamed(
-                                  context, '/displayrecipeScreen');
-                              _removeOverlay();
-                            },
-                            color: subColor),
-                      ],
-                    )
-                  : const Column(),
-              buildMenuOption(
-                icon: Icons.book_sharp,
-                label: 'Book of Exuses',
-                onPressed: () {
-                  Navigator.pushReplacementNamed(
-                      context, '/bookofexusesScreen');
-                  _removeOverlay();
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                    ),
+                  ),
+                ),
+              ],
+            ));
   }
 
   Widget buildMenuOption({
